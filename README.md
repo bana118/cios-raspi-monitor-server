21ee9bfae4400c116d35d14ffd0a3232033745d0
banatech <43720583+bana118@users.noreply.github.com>
2021-06-02 12:34:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# go-container-template
+Template of vscode remote development container with Golang.  
---
+++ cmd/example/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	output := example("Hello, world!")
+	fmt.Print(output)
+}
